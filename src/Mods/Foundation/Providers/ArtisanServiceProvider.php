e3f2ba02951af8cf16aecb4839e47af9ce6f781d
Sunel Tr <sunelbe@gmail.com>
2017-09-23 22:01:24 +0530

    removed make cmd as its no longer supported

---
+++ src/Mods/Foundation/Providers/ArtisanServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Mods\Foundation\Providers;
+
+use Illuminate\Console\Scheduling\ScheduleRunCommand;
+use Illuminate\Console\Scheduling\ScheduleFinishCommand;
+use Illuminate\Foundation\Providers\ArtisanServiceProvider as ServiceProvider;
+
+class ArtisanServiceProvider extends ServiceProvider
+{
+
+	/**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'CacheClear' => 'command.cache.clear',
+        'CacheForget' => 'command.cache.forget',
+        'ClearCompiled' => 'command.clear-compiled',
+        'ClearResets' => 'command.auth.resets.clear',
+        'ConfigCache' => 'command.config.cache',
+        'ConfigClear' => 'command.config.clear',
+        'Down' => 'command.down',
+        'Environment' => 'command.environment',
+        'KeyGenerate' => 'command.key.generate',
+        'Migrate' => 'command.migrate',
+        'MigrateFresh' => 'command.migrate.fresh',
+        'MigrateInstall' => 'command.migrate.install',
+        'MigrateRefresh' => 'command.migrate.refresh',
+        'MigrateReset' => 'command.migrate.reset',
+        'MigrateRollback' => 'command.migrate.rollback',
+        'MigrateStatus' => 'command.migrate.status',
+        'Optimize' => 'command.optimize',
+        'QueueFailed' => 'command.queue.failed',
+        'QueueFlush' => 'command.queue.flush',
+        'QueueForget' => 'command.queue.forget',
+        'QueueListen' => 'command.queue.listen',
+        'QueueRestart' => 'command.queue.restart',
+        'QueueRetry' => 'command.queue.retry',
+        'QueueWork' => 'command.queue.work',
+        'RouteCache' => 'command.route.cache',
+        'RouteClear' => 'command.route.clear',
+        'RouteList' => 'command.route.list',
+        'Seed' => 'command.seed',
+        'ScheduleFinish' => ScheduleFinishCommand::class,
+        'ScheduleRun' => ScheduleRunCommand::class,
+        'StorageLink' => 'command.storage.link',
+        'Up' => 'command.up',
+        'ViewClear' => 'command.view.clear',
+    ];
+
+	/**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $devCommands = [
+        'AppName' => 'command.app.name',
+        'CacheTable' => 'command.cache.table',
+        'EventGenerate' => 'command.event.generate',
+        'NotificationTable' => 'command.notification.table',
+        'QueueFailedTable' => 'command.queue.failed-table',
+        'QueueTable' => 'command.queue.table',
+        'SessionTable' => 'command.session.table',
+        'Serve' => 'command.serve',
+    ];
+
+}
--- src/Mods/Foundation/register.php
+++ src/Mods/Foundation/register.php
@@ -24,7 +24,7 @@ return [
             Illuminate\Broadcasting\BroadcastServiceProvider::class,
             Illuminate\Bus\BusServiceProvider::class,
             Illuminate\Cache\CacheServiceProvider::class,
-            Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+            Mods\Foundation\Providers\ConsoleSupportServiceProvider::class,
             Illuminate\Cookie\CookieServiceProvider::class,
             Illuminate\Database\DatabaseServiceProvider::class,
             Illuminate\Encryption\EncryptionServiceProvider::class,
